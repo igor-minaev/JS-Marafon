5f192d46236a354e5d45315d0610e3fd3861b157
igor-minaev <igor.minayeu@gmail.com>
2021-10-31 22:44:13 +0300

    fix

--- Week 3/Day 14/Calculator/script.js
+++ Week 3/Day 14/Calculator/script.js
@@ -1,3 +1,4 @@
+//Version 1
 let a = +prompt('a:','');
 let b = +prompt('b','');
 let operation = prompt('Оператор:', 'sum - "+",diff - "-", multi - "*", divi - "/", rem - "%", exp - "**"');
@@ -24,3 +25,32 @@ function Calc(operation,a,b) {
 
 let result = Calc(operation,a,b);
 alert(result);
+
+
+// Version 2
+/* function Calc(operation,a,b) {
+  if (!operation || !a || !b || typeof a != 'number' || typeof b != 'number' ) {
+    return 'Error';
+  }  else if (operation == 'sum') {
+    return a + b;
+  } else if (operation == 'diff') {
+    return a - b;
+  } else if (operation == 'multi') {
+    return a * b;
+  } else if (operation == 'divi') {
+    return a / b;
+  } else if (operation == 'rem') {
+    return a % b;
+  } else if (operation == 'exp') {
+    return a ** b;
+  } else {
+    return 'unknown operation';
+  }
+}
+
+
+console.log(Calc('diff',1,3));
+console.log(Calc('diff',true,3));
+console.log(Calc('diff','r',3));
+console.log(Calc(1,2,3));
+console.log(Calc('diff','r','')); */
