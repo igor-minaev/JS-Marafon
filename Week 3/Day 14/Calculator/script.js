68b2dd516c7a427487f1d0f571bf5342e4e5a73e
igor-minaev <igor.minayeu@gmail.com>
2021-11-02 21:50:22 +0300

    fix calculator

--- Week 3/Day 14/Calculator/script.js
+++ Week 3/Day 14/Calculator/script.js
@@ -1,27 +1,40 @@
 
 // Version 1
 function Calc(operation,a,b) {
-  const isNotValid = (!operation || a == 'undefined' ||  b =='undefined' || typeof a !== 'number' || typeof b !== 'number' );
+  const isNotValid = (!operation || a == 'undefined' ||  b =='undefined' || typeof a !== 'number' || typeof b !== 'number' || b === 0 );
 
-  if (isNotValid ) {
-    return 'Error';
-  }  else if (operation === 'sum') {
-    return a + b;
-  } else if (operation === 'diff') {
-    return a - b;
-  } else if (operation === 'multi') {
-    return a * b;
-  } else if (operation === 'divi') {
-    return a / b;
-  } else if (operation === 'rem') {
-    return a % b;
-  } else if (operation === 'exp') {
-    return a ** b;
-  } else {
-    return 'unknown operation';
+  if (isNotValid) {
+    return "Error";
   }
-}
 
+  switch (operation) {
+    case "sum":
+      return a + b;
+      break;
+
+    case "diff":
+      return a - b;
+      break;
+
+    case "multi":
+      return a * b;
+      break;
+
+    case "divi":
+      return a / b;
+      break;
+
+    case "rem":
+      return a % b;
+      break;
+
+    case "exp":
+      return a ** b;
+
+    default:
+      return "unknown operation";
+    }
+}
 
 console.log(Calc('diff',1,3));
 console.log(Calc('diff',true,3));
