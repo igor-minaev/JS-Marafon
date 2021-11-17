ee9f7cb3fd5d8d907a9e7bbae698126aa116cb7b
igor-minaev <igor.minayeu@gmail.com>
2021-11-17 22:53:09 +0300

    add 27 day of marafon

---
+++ Week 5/Day 27/script.js
@@ -0,0 +1,40 @@
+/* let num1 = +prompt('Введите первое число:', '');
+let num2 = +prompt('Введите второе число:', '');
+alert(num1 + num2); */
+
+/* console.log(Math.round(6.35 * 10) / 10); */
+
+/* function readNumber() {
+  let num;
+  do {
+    num = prompt("Введите число");
+  } while (!isFinite(num));
+
+  if (num === null || num === "") return null;
+  return +num;
+}
+
+alert(`Число: ${readNumber()}`); */
+
+/* let i = 0;
+while (i != 10) {
+  i += 0.2;
+  console.log(i);
+}
+ */
+
+/* function random(min, max) {
+console.log(min + Math.random() * (max - min));
+}
+
+random(1, 4); */
+
+/* function randomInteger(min, max) {
+  console.log(Math.floor(min + Math.random() * (max + 1 - min)));
+};
+
+randomInteger(2, 6);
+randomInteger(2, 6);
+randomInteger(2, 6);
+randomInteger(2, 6);
+randomInteger(2, 6); */
